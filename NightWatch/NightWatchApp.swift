4a92ffceb88fd21c3a774d545cd2ff82f6a9198d
Ali Akber <ali852609@gmail.com>
2022-10-16 22:59:41 +0600

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by BS274 on 16/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
