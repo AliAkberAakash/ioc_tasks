a7414f670e012bb5f9d73ad86a5685561f5f9d60
Ali Akber <ali852609@gmail.com>
2022-10-17 18:17:02 +0600

    add list

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var nightlyTasks = [
+        "Clean floors",
+        "Lock doors",
+        "Turn off lights",
+        "Keep watch",
+        "Monitor night camera",
+        "Walk around the premises",
+    ]
+    
+    var weeklyTasks =  [
+        "Clean the kitchen",
+        "Wash the bathroom",
+        "Varnish chairs",
+    ]
+    
+    var monthlyTasks = [
+        "Trim bushes",
+        "Mow the lawns"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        List{
+            Section(header: HStack {
+                Image(systemName: "moon.stars")
+                Text("Nightly Tasks")
+            }.font(.title3)
+            ){
+                ForEach(nightlyTasks,id:\.self, content: {
+                        taskName in
+                        Text(taskName)
+                    }
+                )
+            }
+            
+            Section(header: HStack {
+                Image(systemName: "sunrise")
+                Text("Weekly Tasks")
+            }.font(.title3)
+            ){
+                ForEach(weeklyTasks,id:\.self, content: {
+                        taskName in
+                        Text(taskName)
+                    }
+                )
+            }
+            
+            Section(header: HStack {
+                Image(systemName: "calendar")
+                Text("Monthly Tasks")
+            }.font(.title3)
+            ){
+                ForEach(monthlyTasks,id:\.self, content: {
+                        taskName in
+                        Text(taskName)
+                    }
+                )
+            }
+        }.listStyle(GroupedListStyle())
     }
 }
 
